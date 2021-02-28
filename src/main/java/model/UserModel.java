d4b982e4c7edb6dc48515b396fb0939ef030d17a
DuyTran1811 <ferssken99@gmail.com>
2021-02-28 19:16:45 +0700

    Create Model

---
+++ src/main/java/model/UserModel.java
@@ -0,0 +1,50 @@
+package model;
+
+public class UserModel extends AbstractModel<UserModel> {
+    private String userName;
+    private String passWord;
+    private String fullName;
+    private int status;
+    private Long roleId;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+}
